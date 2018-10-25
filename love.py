04225d79dea55a3e925b1a13a3c3a406766632f4
zyksir <30949016+zyksir@users.noreply.github.com>
2018-10-25 18:12:49 +0800

    Add files via upload

---
+++ love.py
@@ -0,0 +1,29 @@
+
+# -*- coding: UTF-8 -*-
+
+
+heart = """
+   
+                L       OOO    V   V  EEEEE
+                L      O   O   V   V  E    
+                L      O   O   V   V  EEE  
+                L      O   O    V V   E    
+    oo          LLLLL   OOO      V    EEEEE
+       oo                                        
+          oo     OOOOOOOO:       OOOOOOOO!       
+             oOOOO!!!!;;;;O    OO.......:;!O     
+            'OOO!!!;;;;;;;;O  O.......:   ;!O    
+            OOO!!!!;;::::::.OO........:    ;!O   
+            OO!!!!;;:::::..............:   ;!O   
+            OOO!!!;::::::..............:   ;!O   
+             OO!!;;::::::.............:   ;!O           
+              OO!;;::::::......oo.....::::!O     
+                O!!;::::::........oo..:::O                  
+                  !!!;:::::..........ooO         		
+                     !!;:::::.......O   oo              
+                       ;;::::.....O        oo  ,o 			
+                          :::..O              ooo 			waiting for you to come back
+                            ::.              oooo 			(｡･ω･｡)ﾉ♡
+                             :                   
+"""
+print(heart)
